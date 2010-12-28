5a8b8a29dcca5ecb4a406ab41cdbebb3a6ddabdd
Dru Sellers <dru@drusellers.com>
2010-12-27 18:14:26 -0600

    moving things around

---
+++ tosca.server/src/tosca/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tosca::Application.config.secret_token = '4b471bbdae8abd0e80c2302e5e8e50c4b6544fca7b95c65445dcfd799eb667f82bfc28ea0b104a5cd235646ae58b330bc2c3d2b512be7e2eed578ddd6a00d3dd'
